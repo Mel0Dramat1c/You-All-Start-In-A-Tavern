89eb88c49fc3ab9188af0f043897c039b68ae831
Mel Maguire <reishyn616@gmail.com>
2024-03-12 21:17:32 +0000

    create apps

--- you_all_start_in_a_tavern/settings.py
+++ you_all_start_in_a_tavern/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hxzg9fl6-%h_#-2@nxmmrni0$rm@_-5zig5na2*5iz_^!m+^$h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com', 'heroku.com']
 
 
 # Application definition
