f836eab715bcb5c367bd393ba716d16505c3c24c
Mel Maguire <reishyn616@gmail.com>
2024-03-13 21:30:28 +0000

    Add cloudinary to installed apps

--- you_all_start_in_a_tavern/settings.py
+++ you_all_start_in_a_tavern/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloundinary_storage',
+    'cloudinary',
     'the_tavern',
     'profiles',
 ]
