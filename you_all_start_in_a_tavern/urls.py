6ea06813af1e0136feb9266610335de04a47567a
Mel Maguire <reishyn616@gmail.com>
2024-03-13 22:24:19 +0000

    Remove vestigal comments

--- you_all_start_in_a_tavern/urls.py
+++ you_all_start_in_a_tavern/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-## requires fixing !!!
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("the_tavern.urls")),
