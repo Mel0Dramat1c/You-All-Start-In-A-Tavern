35adef37618fb061582bdf4329af8c469dd79eb6
Mel Maguire <reishyn616@gmail.com>
2024-03-15 20:14:10 +0000

    Add summernotemodeladmin to the_tavern admin

--- the_tavern/admin.py
+++ the_tavern/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from characters.models import Character, CharacterClass, CharacterRace
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
 admin.site.register(Character)
