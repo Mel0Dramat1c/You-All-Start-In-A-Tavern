7ae5cf9778a003642e2657d4228cce3a7399bbb4
Mel Maguire <reishyn616@gmail.com>
2024-03-19 12:45:23 +0000

    Implement CRUD functionality

--- characters/urls.py
+++ characters/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.main_page, name="home"),
-    path("create", views.create, name="character_create"),
-    path("<int:id>", views.detail, name="character_detail"),
+    path("create/", views.create, name="character_create"),
+    path("edit/<int:id>/", views.edit, name="character_edit"),
+    path("<int:id>/", views.detail, name="character_detail"),
+    path("delete/<int:id>/", views.delete, name="character_delete"),
 ]
--- characters/views.py
+++ characters/views.py
@@ -1,12 +1,13 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Character
 from characters.forms import CharacterForm
 
 
 def main_page(request):
     characters = Character.objects.all()
-    return render(request, 'main_page.html', {'characters': characters})
+    return render(request, 'the_tavern/main_page.html', {'characters': characters})
 
 
 def detail(request, id):
@@ -18,14 +19,41 @@ def detail(request, id):
 
 
 def create(request):
+    form = CharacterForm(request.POST or None)
     if request.method == 'POST':
-        form = CharacterForm(request.POST)
         if form.is_valid():
             character = form.save(commit=False)
             character.profile = request.user.profile
             character.save()
-            # Redirect to character detail page
-            return redirect('character_detail', id=character.id)
-    else:
-        form = CharacterForm()
+            messages.success(request, "Your character has been created!")
+            # Redirect to character page
+            return redirect(reverse('character_detail', kwargs={'id': character.id}))
+        messages.error(request, "Error! Please try again.")
     return render(request, 'create_a_character.html', {'form': form})
+
+
+def edit(request, id):
+    character = get_object_or_404(Character, id=id)
+    if not character.profile.user == request.user:
+        messages.error(request, "Error. Invalid credentials.")
+        return redirect('home')
+    
+    form = CharacterForm(request.POST or None, instance=character)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your character has been updated!")
+            # Redirect to character page
+            return redirect(reverse('character_detail', kwargs={'id': id}))
+        messages.error(request, "Error! Please try again.")
+    return render(request, 'edit_a_character.html', {'form': form, 'character': character})
+
+
+def delete(request, id):
+    character = get_object_or_404(Character, id=id)
+    if not character.profile.user == request.user:
+        messages.error(request, "Error. Invalid credentials.")
+        return redirect('home')
+    character.delete()
+    messages.success(request, "Your character has been deleted!")
+    return redirect('home')
