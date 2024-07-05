444ba2d519bd52804dd1d6644d89c36ede04b016
Mel Maguire <reishyn616@gmail.com>
2024-07-05 13:56:52 +0000

    Fix: Remove the last of excessive comment code

--- you_all_start_in_a_tavern/wsgi.py
+++ you_all_start_in_a_tavern/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for you_all_start_in_a_tavern project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
